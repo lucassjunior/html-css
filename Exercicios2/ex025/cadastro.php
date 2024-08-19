ee8c22ef3ca0c82f46847205448b148f8605548c
lucasjmmm <luucas.l.junior@gmail.com>
2024-08-19 09:20:49 -0300

    aulas.

--- Exercicios2/ex025/cadastro.php
+++ Exercicios2/ex025/cadastro.php
@@ -1,2 +1,14 @@
-Os dados foram enviados!
-Aprenda PHP para saber oque fazer com eles.
+<?php
+$servername = "localhost";
+$username = "root"; // Cambia esto según tu configuración
+$password = ""; // Cambia esto según tu configuración
+$dbname = "test_db"; // Cambia esto según tu base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
