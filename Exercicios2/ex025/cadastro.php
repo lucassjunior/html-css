f31875d2631b9512076fb21da2f6d7cc8232f8e6
lucasjmmm <luucas.l.junior@gmail.com>
2024-08-27 10:19:07 -0300

    .

--- Exercicios2/ex025/cadastro.php
+++ Exercicios2/ex025/cadastro.php
@@ -1,14 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root"; // Cambia esto según tu configuración
-$password = ""; // Cambia esto según tu configuración
-$dbname = "test_db"; // Cambia esto según tu base de datos
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-?>
+Bom dia
